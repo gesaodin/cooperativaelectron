73b3487b4b4b814a236a653f955d4b9b06014ee1
judelvis <jud.prog@gmail.com>
2015-07-17 15:08:29 -0430

    reporte de estatus de recepcion de documentos

---
+++ system/js/estatico/planCorporativo.js
@@ -0,0 +1,1 @@
+var Esq_planCorporativo={"campos":[{"id":"plan","tipo":"texto","estilo":"width:100%;","etiqueta":"Plan","requerido":true},{"id":"porcentaje","tipo":"texto","estilo":"width:100%;","etiqueta":"Porcentaje","requerido":true}],"botones":[{"id":"btnGuardar","tipo":"submit","clase":"icon8","etiqueta":"Guardar"}],"fconsulta":"","fguardar":"guardaPlan","titulo":"Registrar Documentos"}
