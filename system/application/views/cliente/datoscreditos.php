cd1af342253c88881c01eced0929d0a65cff54d2
root <root@canaima-popular.(none)>
2015-08-05 12:28:34 -0430

    Fechas de solicitud

--- system/application/views/cliente/datoscreditos.php
+++ system/application/views/cliente/datoscreditos.php
@@ -23,7 +23,7 @@
 			
 			<input type="text" name="txtDiaC"   id="txtDiaC"style="width: 55px;" disabled='true' value =<?php echo (int)$fecha_actual[0]; ?> />
 			<input type="text" name="txtMesC"	id="txtMesC"style="width: 55px;" disabled='true' value =<?php echo (int)$fecha_actual[1]; ?> />
-			<input type="text" name="txtAnoC"	id="txtAnoC"style="width: 60px;" disabled='true' value =<?php echo $fecha_actual[2]; ?> />
+			<input type="text" name="txtAnoC"	id="txtAnoC"style="width: 60px;" disabled='true' value =<?php echo (int)$fecha_actual[2]; ?> />
 		<?php }else{?>
 			<select name="txtDiaC" id="txtDiaC"style="width: 55px;">
 				<option value=0>Dia:</option>
@@ -39,7 +39,7 @@
 			</select>
 			<select name="txtAnoC"	id="txtAnoC"style="width: 60px;">
 				<option value=0>A&ntilde;o:</option>
-				<?php 	for($i = 2015; $i <= 2017; $i++){		?>
+				<?php 	for($i = 2012; $i <= 2018; $i++){		?>
 					<option value='<?php echo $i ?>'><?php echo $i ?></option>
 				<?php	}	?>
 			</select>
